3f221829fb2fc017bd3fa17bdb371ef8b508195a
Thomas Mayrhofer <thomas@mayrhofer.at>
2018-11-20 22:51:27 +0100

    Toggles isFocused onFocus and onBlur.

--- src/TextInput.spec.js
+++ src/TextInput.spec.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import buildInput from './build-input';
-import renderer from 'react-test-renderer';
-import { assertThat, hasProperty, equalTo } from 'hamjest';
+import { assertThat, hasProperties, equalTo } from 'hamjest';
+
+import { mount, render, shallow } from "enzyme";
 
 test('passes props to input', () => {
-    const props = { onChange: () => {} }
-    const MyInput = buildInput(({ Input }) => (
+    const MyInput = buildInput(({ Input, ...props }) => (
         <Input { ...props } />
-    ))
-    const component = renderer.create(<MyInput />);
-    assertThat(component.toJSON(), hasProperty('props', equalTo(props)))   
+    ));
+    
+    const input = mount(<MyInput autoComplete="true"/>).find('input');
+    assertThat(input.props(), hasProperties({ autoComplete: 'true' }));
 });
+
+
+describe('isFocused', () => {
+    test('false, initially', () => {
+        const MyInput = buildInput(({ Input }) => <Input />);
+        const wrapper = mount(<MyInput />);
+        assertThat(wrapper.children().props(), hasProperties({ isFocused: false }));    
+    });
+
+    test('true, when focused', () => {
+        const MyInput = buildInput(({ Input }) => <Input />);
+        const wrapper = mount(<MyInput />);
+        wrapper.find('input').simulate('focus');
+        assertThat(wrapper.children().props(), hasProperties({ isFocused: true }));    
+    });
+
+    test('false, when focused and blured afterwards', () => {
+        const MyInput = buildInput(({ Input }) => <Input />);
+        const wrapper = mount(<MyInput />);
+        wrapper.find('input').simulate('focus');
+        wrapper.find('input').simulate('blur');
+        assertThat(wrapper.children().props(), hasProperties({ isFocused: false }));    
+    });
+})
--- src/build-input.js
+++ src/build-input.js
@@ -1,30 +1,51 @@
 import React from 'react';
 
-const buildInputHTMLElement = (registerRef) => (props) => (
+const omit = (attributes, obj) => Object.keys(obj).reduce((acc, key) => {
+    if (!attributes.includes(key)) { acc[key] = obj[key]; }
+    return acc;
+}, {});
+
+const buildInputHTMLElement = ({ ref, excludeProps, ...wrapperProps }) => (props) => (
     <input 
-        {...props} 
-        ref={ registerRef } 
+        ref={ ref }
+        { ...(omit(excludeProps, props)) }
+        { ...wrapperProps }
     />
 );
-
 const buildInput = (InputComponent) => {
     return class InputState extends React.Component {
         constructor(props) {
             super(props);
             this.ref = React.createRef();
+            this.state = {
+                isFocused: false,
+            }
         }
         get domElements() {
             if (!this._domElements) { 
                 this._domElements = {
-                    Input: buildInputHTMLElement(this.ref) 
+                    Input: buildInputHTMLElement({
+                        ref: this.ref,
+                        excludeProps: Object.keys(this.state),
+                        onFocus: this.onFocus,
+                        onBlur: this.onBlur,
+                    }) 
                 };
             }
             return this._domElements;            
-        } 
+        }
 
+        onFocus = () => this.setState((state) => ({ ...state, isFocused: true }));
+        onBlur = () => this.setState((state) => ({ ...state, isFocused: false }));
         
         render() {
-            return <InputComponent { ...this.props} { ...this.domElements } />
+            return (
+                <InputComponent
+                    { ...this.props } 
+                    { ...this.state }
+                    { ...this.domElements } 
+                />
+            );
         }
     }
 } 
