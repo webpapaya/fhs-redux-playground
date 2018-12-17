bb7947cde57d980eb6e05a6dfe9605762fdcaa41
Thomas Mayrhofer <thomas@mayrhofer.at>
2018-12-18 00:47:39 +0100

    Added money_transaction_reports.

---
+++ migrations/R__money_transaction_reports.sql
@@ -0,0 +1,77 @@
+-- I know I'm ugly...
+
+CREATE OR REPLACE VIEW money_transaction_reports as 
+  select sum(amount) as amount, date_trunc('day', created_at) as date, 'day' as granularity, debitor_id, creditor_id
+  from money_transactions
+  group by date_trunc('day', created_at), debitor_id, creditor_id
+
+  UNION 
+
+  select sum(amount) as amount, date_trunc('day', created_at) as date, 'day' as granularity, null as debitor_id, null as creditor_id
+  from money_transactions
+  group by date_trunc('day', created_at), debitor_id, creditor_id
+
+  UNION
+
+  select sum(amount) as amount, date_trunc('day', created_at) as date, 'day' as granularity, debitor_id, null as creditor_id
+  from money_transactions
+  group by date_trunc('day', created_at), debitor_id, creditor_id
+
+  UNION
+
+  select sum(amount) as amount, date_trunc('day', created_at) as date, 'day' as granularity, null as debitor_id, creditor_id
+  from money_transactions
+  group by date_trunc('day', created_at), debitor_id, creditor_id
+  
+  UNION
+
+  -- MONTH
+
+  select sum(amount) as amount, date_trunc('month', created_at) as date, 'month' as granularity, debitor_id, creditor_id
+  from money_transactions
+  group by date_trunc('month', created_at), debitor_id, creditor_id
+
+  UNION 
+
+  select sum(amount) as amount, date_trunc('month', created_at) as date, 'month' as granularity, null as debitor_id, null as creditor_id
+  from money_transactions
+  group by date_trunc('month', created_at), debitor_id, creditor_id
+
+  UNION
+
+  select sum(amount) as amount, date_trunc('month', created_at) as date, 'month' as granularity, debitor_id, null as creditor_id
+  from money_transactions
+  group by date_trunc('month', created_at), debitor_id, creditor_id
+
+  UNION
+
+  select sum(amount) as amount, date_trunc('month', created_at) as date, 'month' as granularity, null as debitor_id, creditor_id
+  from money_transactions
+  group by date_trunc('month', created_at), debitor_id, creditor_id
+
+  UNION
+
+  -- YEAR
+
+  select sum(amount) as amount, date_trunc('year', created_at) as date, 'year' as granularity, debitor_id, creditor_id
+  from money_transactions
+  group by date_trunc('year', created_at), debitor_id, creditor_id
+
+  UNION 
+
+  select sum(amount) as amount, date_trunc('year', created_at) as date, 'year' as granularity, null as debitor_id, null as creditor_id
+  from money_transactions
+  group by date_trunc('year', created_at), debitor_id, creditor_id
+
+  UNION
+
+  select sum(amount) as amount, date_trunc('year', created_at) as date, 'year' as granularity, debitor_id, null as creditor_id
+  from money_transactions
+  group by date_trunc('year', created_at), debitor_id, creditor_id
+
+  UNION
+
+  select sum(amount) as amount, date_trunc('year', created_at) as date, 'year' as granularity, null as debitor_id, creditor_id
+  from money_transactions
+  group by date_trunc('year', created_at), debitor_id, creditor_id
+;
