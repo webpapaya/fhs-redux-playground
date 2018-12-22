ff1fd7e7f045b7f5ddf33fde64f7bc7c363545cc
Thomas Mayrhofer <thomas@mayrhofer.at>
2018-12-22 21:42:06 +0100

    Added navigation and styles loader.

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import styles from './button.css';
 
-const Component = ({ type = 'button', disabled, children, onClick }) => (
-  <button type={type} disabled={disabled} onClick={onClick}>
+
+const Component = ({ 
+  type = 'button', 
+  disabled, 
+  children, 
+  onClick,
+  variant='filled' 
+}) => (
+  <button 
+    type={type} 
+    disabled={disabled} 
+    onClick={onClick}
+    className={`${styles.button} ${styles[variant]}`}
+  >
     { children }
   </ button> 
 );
--- src/container/navigation/organism.js
+++ src/container/navigation/organism.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './organism.css';
+import Button from '../../components/button';
 
 const Organism = ({ onUserSignOut }) => (
-    <div>
-        <button onClick={onUserSignOut}>Sign out</button>
-    </div>
+    <nav className={styles.wrapper}>
+        My friend owes me
+        <Button onClick={onUserSignOut} variant="outlined">Sign out</Button>
+    </nav>
 )
 
 export default Organism;
