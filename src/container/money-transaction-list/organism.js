d5a5f22a7721f5d6bc532823ce071f7154236375
Thomas Mayrhofer <thomas@mayrhofer.at>
2018-12-22 22:02:05 +0100

    Added base styles.

--- src/container/money-transaction-list/organism.js
+++ src/container/money-transaction-list/organism.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Button from '../../components/button';
+import styles from './organism.css';
 
 export default ({ moneyTransactions, users, onDestroy }) => (
-    <ul>
+    <ul className={styles.wrapper}>
         { moneyTransactions.map(({ id, creditorId, debitorId, amount }) => (
-            <li key={id}>
+            <li key={id} className={styles.row}>
                 { (users.find((user) => user.id === creditorId ) || {}).name } - 
                 { (users.find((user) => user.id === debitorId ) || {}).name } - 
                 { amount }
