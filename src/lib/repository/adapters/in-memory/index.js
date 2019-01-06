5a22c907d797a42bc8ea0ebaaf7c5080654c456b
Thomas Mayrhofer <thomas@mayrhofer.at>
2019-01-06 23:24:39 +0100

    Verfiy interface and cleanup.

--- src/lib/repository/adapters/in-memory/index.js
+++ src/lib/repository/adapters/in-memory/index.js
@@ -1,52 +1,6 @@
-import { filterByQuery } from '../../selectors';
+export { default as buildRepository } from './build-repository';
 
-export const buildRepository = ({ resource }) => {
-  const where = (connection, query) => {
-    const records = connection[resource];
-    return filterByQuery(query, records);
-  }
-
-  const destroy = (connection, query) => {
-    const destroyedRecords = where(connection, query);
-    
-    // There is room for performance improvements here =)
-    connection[resource] = connection[resource].filter((record) => {
-      return !destroyedRecords.includes(record);
-    });
-    
-    return destroyedRecords;
-  }
-
-  const create = (connection, record) => {
-    connection[resource] = [...connection[resource], record];
-    return  record;
-  }
-
-  const update = (connection, query, values) => {
-    const recordsToUpdate = where(connection, query);
-    const recordsToReturn = [];
-
-    // There is room for performance improvements here =)
-    connection[resource] = connection[resource].map((record) => {
-      if (!recordsToUpdate.includes(record)) { return record; }
-      const updatedRecord = { ...record, ...values };
-      recordsToReturn.push(updatedRecord);
-      return updatedRecord;
-    });
-
-    return recordsToReturn;
-  }
-
-  const count = (connection, query) => {
-    return where(connection, query).length;
-  }
-
-  return {
-    count,
-    where,
-    create,
-    destroy,
-    update,
-  };
-}
-
+const noop = () => {};
+export const setAuthentication = noop;
+export const unsetAuthentication = noop;
+export const buildConnection = noop;
