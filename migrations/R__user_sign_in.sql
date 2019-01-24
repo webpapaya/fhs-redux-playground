ebaf4eb50f99c980a05d170129c5c81b90194a25
Thomas Mayrhofer <thomas@mayrhofer.at>
2019-01-24 23:30:40 +0100

    Add userId to jwt.

--- migrations/R__user_sign_in.sql
+++ migrations/R__user_sign_in.sql
@@ -1,28 +1,30 @@
 CREATE TYPE public.jwt_token AS (
-  token text
+	token text
 );
 
 -- login should be on your exposed schema
 create or replace function
 user_sign_in(email text, pass text) returns public.jwt_token as $$
 declare
-  _role name;
-  result public.jwt_token;
+  _userId integer;
+	_role name;
+	result public.jwt_token;
 begin
-  -- check email and password
-  select basic_auth.user_role(email, pass) into _role;
+	-- check email and password
+	select basic_auth.user_role(email, pass) into _role;
   if _role is null then
 	raise invalid_password using message = 'invalid user or password';
-  end if;
+	end if;
+  select id from public.users where role = _role limit 1 into _userId;
 
-  select basic_auth.sign(
-	  row_to_json(r), 'reallyreallyreallyreallyverysafe'
+	select basic_auth.sign(
+		row_to_json(r), 'reallyreallyreallyreallyverysafe'
 	) as token
 	from (
-	  select _role as role, user_sign_in.email as email,
+		select _role as role, user_sign_in.email as email, _userId as id,
 		 extract(epoch from now())::integer + 60*60000 as exp
 	) r
 	into result;
-  return result;
+	return result;
 end;
 $$ language plpgsql SECURITY DEFINER;
