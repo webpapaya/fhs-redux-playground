14ce19263a499e4d59acca22871aa6ededfde5bb
Thomas Mayrhofer <thomas@mayrhofer.at>
2019-01-24 22:55:37 +0100

    Only the currently authenticated user can update himself.

--- migrations/R__policies.sql
+++ migrations/R__policies.sql
@@ -10,3 +10,13 @@ USING (
   debitor_id in (select id from users where role = current_user) OR
   creditor_id in (select id from users where role = current_user));
 
+ALTER TABLE public.users ENABLE ROW LEVEL SECURITY;
+
+DROP POLICY IF EXISTS user_policy
+ON public.users;
+
+CREATE POLICY user_policy
+ON public.users
+FOR ALL
+USING (role = current_user);
+
